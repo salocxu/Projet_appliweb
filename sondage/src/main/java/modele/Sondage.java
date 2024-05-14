c8a1210c4643458a962ac3cbc563f5d79f9c7df9
nlermusi <nicolas.lermusiaux@etu.inp-toulouse.fr>
2024-05-14 19:02:26 +0200

    j'ai push

--- sondage/src/main/java/modele/Sondage.java
+++ sondage/src/main/java/modele/Sondage.java
@@ -1,8 +1,8 @@
 package modele;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
 
